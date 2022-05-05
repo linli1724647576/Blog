76e011753164e88654637062e81f75f247b2e0d6
linli <1724647576@qq.com>
2022-05-05 17:41:02 +0800

    2022.5.5

---
+++ src/main/java/com/ll/service/CommentService.java
@@ -0,0 +1,15 @@
+package com.ll.service;
+
+import com.ll.po.Comment;
+
+import java.util.List;
+
+/**
+ * Created by limi on 2017/10/22.
+ */
+public interface CommentService {
+
+    List<Comment> listCommentByBlogId(Long blogId);
+
+    Comment saveComment(Comment comment);
+}
---
+++ src/main/java/com/ll/service/UserService.java
@@ -0,0 +1,11 @@
+package com.ll.service;
+
+import com.ll.po.User;
+
+/**
+ * Created by limi on 2017/10/15.
+ */
+public interface UserService {
+
+    User checkUser(String username, String password);
+}
